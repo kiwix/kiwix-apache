c3d402e112f4c4df6ecb8aad6db8f37076ea9aa6
Julian Harty <julianharty@gmail.com>
2017-04-06 16:46:50 +0100

    One small step for me, and a minor improvement in the functionality.
    1. Migrated from zimlib to kiwixlib[a]
    2. Display the default page of the zim file, if available
    
    [a] Currently we have to manually build kiwixlib and then configure the
    path to it in order to use it. At some point it'll have a way to install it
    then we can tidy up the build script.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 set -e
-g++ `pkg-config --cflags apr-1` -fPIC -DPIC -I/usr/include/apache2/ -c mod_example.cpp
-sudo apxs -i -n example_module `pkg-config --libs apr-1` -lzim -c mod_example.o
+
+export PKG_CONFIG_PATH=/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/lib/x86_64-linux-gnu
+g++ `pkg-config --cflags apr-1 kiwix` -fPIC -DPIC -I/usr/include/apache2/ -I/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/include/ -c mod_example.cpp
+sudo apxs -i -n example_module `pkg-config --libs apr-1 kiwix` -c mod_example.o
--- mod_example.cpp
+++ mod_example.cpp
@@ -6,6 +6,7 @@
 #include <zim/file.h>
 #include <zim/fileiterator.h>
 #include <iostream>
+#include <kiwix/reader.h>
 
 static void register_hooks(apr_pool_t *pool);
 static int example_handler(request_rec *r);
@@ -24,20 +25,35 @@ static int example_handler(request_rec *r) {
 	if (!r->handler || strcmp(r->handler, "example"))
 		return (DECLINED);
 	
-	ap_set_content_type(r, "text/plain");
+	kiwix::Reader *reader = NULL;
+	string zimpath = "/var/www/html/wikipedia.zim";
+	string urlStr = "";
+	bool found = false;
+
+	zim::Article article;
 	
 	try {
-		zim::File f("/var/www/html/wikipedia.zim");
-
-		for(zim::File::const_iterator it = f.begin(); it != f.end(); ++it) {
-			ap_rputs((it->getTitle() + "\n").c_str(), r);
+		zim::File f(zimpath);
+		reader = new kiwix::Reader(zimpath);
+		found = reader->getArticleObjectByDecodedUrl(urlStr, article);
+		if (found) {
+			ap_set_content_type(r, (article.getMimeType()).c_str());
+			string content = string(article.getData().data(), article.getArticleSize()).c_str();
+			content = "<html><head><title>" + article.getTitle() + "</title></head><body>" + content + "</body></html>"; 
+			ap_rputs(content.c_str(), r);
 		}
+		else {
+			ap_set_content_type(r, "text/plain");
+			for(zim::File::const_iterator it = f.begin(); it != f.end(); ++it) {
+				ap_rputs((it->getTitle() + "--\n").c_str(), r);
+			}
+		}
+		reader = NULL;
 	}
 	catch (const std::exception& e) {
 		ap_rputs(e.what(), r);
 	}
 
-	ap_rputs("Hello, CPP world!", r);
 	return OK;
 }
 
