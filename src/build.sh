26c3d591d46a0e895a52ac247eee38e65cce598a
Julian Harty <julianharty@gmail.com>
2017-05-19 14:08:37 +0100

    Revised the build script to separate the location of the kiwix-lib from the rest of the config. This is a first step to improving the build process and getting the build onto Travis-ci.

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 set -e
 
-export PKG_CONFIG_PATH=/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/lib/x86_64-linux-gnu
-g++ --std=c++11  `pkg-config --cflags apr-1 kiwix` -fPIC -DPIC -I/usr/include/apache2/ -I/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/include/ -c mod_kiwix.cpp dealWithOldZimUrls.cpp
+export KIWIX_LIB=/home/julian/myopensourceprojects/kiwix-build/BUILD_native_dyn/INSTALL/
+export PKG_CONFIG_PATH=$KIWIX_LIB/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$KIWIX_LIB/lib/x86_64-linux-gnu
+g++ --std=c++11  `pkg-config --cflags apr-1 kiwix` -fPIC -DPIC -I/usr/include/apache2/ -I$KIWIX_LIB/include/ -c mod_kiwix.cpp dealWithOldZimUrls.cpp
 sudo apxs -i -n kiwix_module `pkg-config --libs apr-1 kiwix` -c mod_kiwix.o dealWithOldZimUrls.o
